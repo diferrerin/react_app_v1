3bf4b6d8cf95b7768c7b2d7bdd13e57db619fae5
Daniel Ferrerin <diferrerin@gmail.com>
2022-07-19 19:02:22 -0300

    Commit 2.NavBar Entrega 2

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
-import NavBar from './components/navBar/NavBar';
+import NavBar from './components/NavBar/NavBar';
 
 //Funcion App principal
 
