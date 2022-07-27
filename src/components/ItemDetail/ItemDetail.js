5c9e4314fcf6572cecb6f639fae243a20a96c22a
Daniel Ferrerin <diferrerin@gmail.com>
2022-07-27 20:18:34 -0300

    Commit 9. Entrega 6 V2 Final funciona OK

--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,10 +1,6 @@
-// 2. Aspectos a incluir en el entregable:
-//ItemDetail.js, que debe mostrar la vista de detalle de un ítem incluyendo su descripción, una foto y el precio.
 
-
-const ItemDetail = (props) => {
+const ItemDetail = ({props}) => {
     const {name,price,stock,img,key, detail} = props;
-
     return(
         <>  
             <div className = "item-product" >
@@ -16,5 +12,4 @@ const ItemDetail = (props) => {
         </>
     )
 }
-
 export default ItemDetail;
--- src/components/ItemDetailContainer/ItemDetailContainer.js
+++ src/components/ItemDetailContainer/ItemDetailContainer.js
@@ -1,9 +1,3 @@
-/*
-1 Consigna: crea tu componente ItemDetailContainer, con la misma premisa que ItemListContainer.
->>Aspectos a incluir en el entregable:
-Al iniciar utilizando un efecto de montaje, debe llamar a un async mock, utilizando lo visto en 
-la clase anterior con Promise, que en 2 segundos le devuelva un 1 ítem, y lo guarde en un estado propio.
-*/
 import { useEffect, useState } from "react";
 import products from "../../utils/product.mock";//Simulamos DB backend
 import ItemDetail from "../ItemDetail/ItemDetail";
@@ -20,13 +14,10 @@ const ItemDetailContainer = ({nroItem})=>{ //Parametro del Item a mostrar detall
             (res)=>{
                 res.forEach(function(element) {// eslint-disable-next-line
                     if (element.id == nroItem) { 
-                        console.log(" Item es Igual a 3 seteamos Item", element); //hasta aca funciona ok (falta timeout)
-                        setItem(element);
+                        console.log(" Item es Igual a 3 seteamos Item", element); 
+                        setTimeout( ()=>{ setItem(element); } , 2000 );
                     }
                   });
-                
-                //let itemRes = res[nroItem];//Busca el id (revisar si funciona ok o hay que agregar algo)
-                //setItem(itemRes); 
             }
         ).catch(  (errorGet) => {  console.log("fallo la llamada a la lista de productos.")  }    )
     } , [] ); // eslint-disable-line react-hooks/exhaustive-deps
